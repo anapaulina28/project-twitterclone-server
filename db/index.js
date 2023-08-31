40228bc48207adec3ac736f6d04daee9153b25a6
John Doe <johndoe@example.com>
2023-08-31 10:00:44 +0100

    initial config

---
+++ db/index.js
@@ -0,0 +1,18 @@
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI= "mongodb+srv://anapaulina28:qCFBP9wlwwqeDPu4@cluster0.19dx1us.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    const dbName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${dbName}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
