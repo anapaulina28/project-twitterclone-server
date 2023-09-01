07c88fafd38f04c5de57f9191885fc51611d4fcf
John Doe <johndoe@example.com>
2023-09-01 10:43:22 +0100

    for test purposes

--- app.js
+++ app.js
@@ -27,5 +27,6 @@ app.use("/auth", authRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 //test
+//test safari paulina
 
 module.exports = app;
